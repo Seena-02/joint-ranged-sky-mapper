13ab072c7bc09b0f02362dd7a2fd25b0e888fe89
seena <seenamohajeran@gmail.com>
2023-12-19 20:56:23 -0800

    fix folders

--- requirements.sh
+++ requirements.sh
@@ -3,15 +3,16 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get insta
+# sudo apt-get insta
 
-pip3 install future
+# pip3 install future
 
-sudo apt-get install screen wxgtk libxml libxslt
-pip3 install pyserial
-pip3 install dronekit
-pip3 install MAVProxy
+# sudo apt-get install screen wxgtk libxml libxslt
+# pip3 install pyserial
+# pip3 install dronekit
+# pip3 install MAVProxy
 
+pip3 install pymavlink
 # NOTE:
 
 
